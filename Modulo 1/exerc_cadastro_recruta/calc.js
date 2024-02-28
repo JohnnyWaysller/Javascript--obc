35c9fa64fd27b56934634bc8381574f256ff0b16
Johnny <johnny_dmg@hotmail.com>
2024-02-27 23:17:48 -0300

    exercUp

---
+++ Modulo 1/exerc_cadastro_recruta/calc.js
@@ -0,0 +1,9 @@
+
+let num1 = parseFloat(prompt("Digite primeiro Numero: "))
+let num2 = parseFloat(prompt("Digite o segundo Numero: "))
+let soma = num1 + num2
+let mult = num1 * num2
+let div = num1 / num2
+let sub = num1 - num2
+
+alert("Resultado: da soma é " + soma + "\n multplicação: "+ mult + "\n Divisao: " + div + "\n subtracao: " + sub)
---
+++ Modulo 1/exerc_cadastro_recruta/index.js
@@ -0,0 +1,10 @@
+// let nome = prompt("Digite seu nome: ")
+// let sobrenome = prompt("Digite sobre nome: ")
+// let estudo = prompt("qual sua area academica: ")
+// let nasc = ""
+// nasc = parseFloat(prompt("qual ano de nascimento"))
+
+// idade = (2024 - nasc)
+// compnome = nome + " " + sobrenome
+// alert("Recruta: " + compnome + " | Idade: " + idade + " | Area: " + estudo)
+
