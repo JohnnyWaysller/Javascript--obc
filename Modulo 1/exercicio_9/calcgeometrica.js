fe5208906a0cf94a06c75bb92ab2460cad42e8e5
Johnny <johnny_dmg@hotmail.com>
2024-03-16 12:23:28 -0300

    updatecorrecao

--- Modulo 1/exercicio_9/calcgeometrica.js
+++ Modulo 1/exercicio_9/calcgeometrica.js
@@ -1,60 +1,62 @@
+
+function triangulo() {
+    const baseTriangulo = Number(prompt("Insira valor da Base"))
+    const alturaTriangulo = Number(prompt("Insira valor da Altura"))
+    return (baseTriangulo * alturaTriangulo) / 2
+}
+function retangulo() {
+    const baseRetangulo = Number(prompt("Insira valor da Base"))
+    const alturaRetangulo = Number(prompt("Insira valor da Altura"))
+    return baseRetangulo * alturaRetangulo
+}
+function quadrado() {
+    const l = Number(prompt("Insira valor do Lado"))
+    return l * l
+}
+function trapezio() {
+    const bMa = Number(prompt("Insira valor da Base Maior"))
+    const bMe = Number(prompt("Insira valor da baseMenor"))
+    const a = Number(prompt("Insira valor da Altura"))
+    return (bMa + bMe) * a / 2
+}
+function circulo() {
+    const r = Number(prompt("Insira valor do raio"))
+    return pi = 3.14 * (r * r)
+}
+function menu() {
+   return prompt("Calculadora geometrica\nSelecione a opcao\ a calcular:\n" +
+        "1. Area do triangulo\n" +
+        "2. Area do retangulo\n" +
+        "3. Area do quadrado\n" +
+        "4. Area do Trapezio\n" +
+        "5. Area do Circulo\n" +
+        "6. Encerrar")
+}
 let opcao = ""
 do{
-        opcao = prompt("Calculadora geometrica\nSelecione a opcao a calcular:\n"+
-        "1. Area do triangulo\n"+
-        "2. Area do retangulo\n"+
-        "3. Area do quadrado\n"+
-        "4. Area do Trapezio\n"+
-        "5. Area do Circulo\n"+
-        "6. Encerrar")
-
-        function triangulo(b,a){
-            return (b * a) / 2
-        }
-        function retangulo(b,a){
-            return b*a
-        }
-        function quadrado(l){
-            return retangulo(l,l)
-        }
-        function trapezio(bm,bm,a){
-            return (bm+bm)*a/2
-        }
-        function circulo(r){
-            return pi=3.14*(r*r)
-        }
-        switch(opcao){
-
-            case "1":
-                const baseTriangulo=Number(prompt("Insira valor da Base"))
-                const alturaTriangulo=Number(prompt("Insira valor da Altura"))
-                alert(triangulo(baseTriangulo,alturaTriangulo))
-                break
-            case "2":
-                const baseRetangulo=Number(prompt("Insira valor da Base"))
-                const alturaRetangulo=Number(prompt("Insira valor da Altura"))
-                alert(triangulo(baseRetangulo,alturaRetangulo))
-                break
-            case "3":
-                const lado=Number(prompt("Insira valor do Lado"))
-                alert(quadrado(lado))
-                break
-            case "4":   
-                const baseMaior=Number(prompt("Insira valor da Base Maior"))
-                const baseMenor=Number(prompt("Insira valor da baseMenor"))   
-                const altura=Number(prompt("Insira valor da Altura"))       
-                alert(trapezio(baseMaior,baseMenor,altura))
-                break
-            case "5":
-                const raio=Number(prompt("Insira valor do raio"))   
-                alert(circulo(raio))
-                break
-            case "6":
-                alert("Encerrando o sistema");
-                break
-            default:
-                alert("Opcao inserida inválida")
-                
-        }
+    opcao=menu()
     
-}while(opcao!=="6")
+    switch (opcao) {
+        case "1":
+            alert(triangulo())
+            break
+        case "2":
+            alert(triangulo())
+            break
+        case "3":
+            alert(quadrado())
+            break
+        case "4":
+            alert(trapezio())
+            break
+        case "5":
+            alert(circulo())
+            break
+        case "6":
+            alert("Encerrando o sistema");
+            break
+        default:
+            alert("Opcao inserida inválida")
+        }
+
+}while(opcao !== "6")
